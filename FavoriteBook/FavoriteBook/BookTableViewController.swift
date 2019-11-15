ab868871c2c2066e1dfabb08360d57d150a1cf8d
Vladislav Boiko <bird.identity@gmail.com>
2019-11-15 13:14:48 +0200

    custome cell style added

--- FavoriteBook/FavoriteBook/BookTableViewController.swift
+++ FavoriteBook/FavoriteBook/BookTableViewController.swift
@@ -35,18 +35,17 @@ class BookTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.bookCell, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.bookCell, for: indexPath) as! BookTableViewCell
         
         let book = books[indexPath.row]
-        cell.textLabel?.text = book.title
-        cell.detailTextLabel?.text = book.description
+        cell.update(with: book)
         
         return cell
     }
     
     // MARK: - Removing books
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCell.EditingStyle.delete {
             books.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
