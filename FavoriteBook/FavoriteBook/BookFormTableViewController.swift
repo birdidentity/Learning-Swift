d429b7b58a51e618ce256767cf8693c43a72c01f
Vladislav Boiko <bird.identity@gmail.com>
2019-11-15 10:57:44 +0200

    replaced BookFormVC with BookFormTVC

--- FavoriteBook/FavoriteBook/BookFormTableViewController.swift
+++ FavoriteBook/FavoriteBook/BookFormTableViewController.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 class BookFormTableViewController: UITableViewController {
+    
+    struct PropertyKeys {
+        static let unwind = "UnwindToBookTable"
+    }
+    
+    var book: Book?
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var authorTextField: UITextField!
@@ -11,7 +17,22 @@ class BookFormTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    func updateView() {
+        guard let book = book else {return}
         
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lengthTextField.text = book.length
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        guard let title = titleTextField.text,
+            let author = authorTextField.text,
+            let genre = genreTextField.text,
+            let length = lengthTextField.text else {return}
+        
+        book = Book(title: title, author: author, genre: genre, length: length)
+        performSegue(withIdentifier: PropertyKeys.unwind, sender: self)
     }
 }
