b45522d58f06ddb15dd2e69cebcf2c845f1aa844
Vladislav Boiko <bird.identity@gmail.com>
2019-11-15 11:07:37 +0200

    fixed issue with saving Books

--- FavoriteBook/FavoriteBook/BookTableViewController.swift
+++ FavoriteBook/FavoriteBook/BookTableViewController.swift
@@ -47,7 +47,7 @@ class BookTableViewController: UITableViewController {
     // MARK: - Navigation
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
-        guard let source = segue.source as? BookFormViewController,
+        guard let source = segue.source as? BookFormTableViewController,
             let book = source.book else {return}
         
         if let indexPath = tableView.indexPathForSelectedRow {
@@ -60,7 +60,7 @@ class BookTableViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let bookFormViewController = segue.destination as? BookFormViewController else {return}
+        guard let bookFormViewController = segue.destination as? BookFormTableViewController else {return}
         
         if let indexPath = tableView.indexPathForSelectedRow,
             segue.identifier == PropertyKeys.editBookSegue {
