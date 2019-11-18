077448dab95dc8f77c3a71de6fb928372257c4b1
Vladislav Boiko <bird.identity@gmail.com>
2019-11-18 11:11:56 +0200

    small fix: deleted delete button in editing mode

--- EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -84,7 +84,9 @@ class EmojiTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
